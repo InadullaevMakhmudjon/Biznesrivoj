6ad04285898cdc51ce0ac3f98298b47a87adc707
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-07 15:32:33 +0500

    Authorntication /login, /logout and /details routes created

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ import auth from '../controllers/auth';
 
 const router = Router();
 
-router.get('/login', auth.login);
-router.get('/hello', (req, res) => res.status(200).json({ message: 'HelloWorld!!' }));
-router.get('/helloAuth', passport.authenticate('jwt', { session: false }), (req, res) => res.status(200).json({ message: 'HelloWorld!!' }));
+router.post('/login', auth.login);
+router.post('/logout', passport.authenticate('jwt', { session: false }), auth.logout);
+router.get('/details', passport.authenticate('jwt', { session: false }), auth.details);
 
 export default router;
