400b34fda147ab5e0d93a484a07541d4848c9d15
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-07 15:28:52 +0500

    passportJs configured

--- configs/passport.js
+++
@@ -1,9 +0,0 @@
-import passport from 'passport';
-import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
-
-require('dotenv').config();
-
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
-  secretOrKey: process.env.JWT_KEY,
-}, (jwtPayload, callBack) => callBack(null, jwtPayload)));
