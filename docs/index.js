581219cfc84431b195916d0aab7ae6943ab8b09c
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-21 15:56:48 +0500

    Swagger docs base url changed

--- docs/index.js
+++ docs/index.js
@@ -2,5 +2,5 @@ import documentation from './swagger.json';
 
 export default {
   ...documentation,
-  host: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'localhost:3030',
+  host: process.env.BASE_URL,
 };
