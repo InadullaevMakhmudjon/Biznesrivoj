4c9bfcde883a2e794ca7f95dcbba7e14134823ee
Hojiakbar <hoji.karimov@rfalab.com>
2020-03-30 01:29:06 +0500

    allow headers

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 
   next();
 });
