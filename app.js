a11008945347c9aa35c44c2f74f9db5a4023ccf3
Hojiakbar <hoji.karimov@rfalab.com>
2020-03-30 01:21:47 +0500

    mehtods removed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
   next();
 });
 
