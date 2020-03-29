fcc116226f84399f7929ac68e9435e0d80f52bcd
Hojiakbar <hoji.karimov@rfalab.com>
2020-03-30 01:15:51 +0500

    content type

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import './config/passport';
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   next();
 });
