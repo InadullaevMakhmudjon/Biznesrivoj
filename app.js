c0af172c85aaba54994ba4073acf0d6d9ae30cda
Hojiakbar <hoji.karimov@rfalab.com>
2020-03-29 20:46:16 +0500

    cors options removed

--- app.js
+++ app.js
@@ -32,12 +32,13 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-  origin: false,
-  methods: ['GET', 'PUT', 'POST'],
-  allowedHeaders: 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
-}));
+app.use(cors());
 
+// {
+//   origin: false,
+//   methods: ['GET', 'PUT', 'POST'],
+//   allowedHeaders: 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+// }
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 indexRouter(app);
 
