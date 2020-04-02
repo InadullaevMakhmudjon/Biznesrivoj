fd857bac1c68830251df15cb3adfb50c1e825a9e
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-02 23:35:31 +0500

    some bugs are being tested

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Request-Headers', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 indexRouter(app);
