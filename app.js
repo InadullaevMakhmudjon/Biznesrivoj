c8d066984a9ff34ba188f786545d8cb8090baf52
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-02 23:39:04 +0500

    new route for testing

--- app.js
+++ app.js
@@ -19,15 +19,7 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Request-Headers', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({ origin: true, credentials: true }));
 
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 indexRouter(app);
