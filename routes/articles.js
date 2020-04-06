cacc1d7bfc565fe050995166e69857b8945dbbc6
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-06 20:34:26 +0500

    Article post for like route created

--- routes/articles.js
+++ routes/articles.js
@@ -9,6 +9,7 @@ import { isNotUser } from '../middlewares/role';
 const router = Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), article.getAll);
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), article.like);
 router.get('/:slug', passport.authenticate('jwt', { session: false }), isNotUser, article.get);
 router.post('/:slug', passport.authenticate('jwt', { session: false }), isNotUser, checkUpdate, validateUpdate, article.update);
 router.post('/', passport.authenticate('jwt', { session: false }), isNotUser, check, validate, article.create);
