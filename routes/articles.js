d0541888939a7d6a8ec5a174792eb9743ad07db8
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-11 19:51:42 +0500

    Article getting by slug opened for default user

--- routes/articles.js
+++ routes/articles.js
@@ -9,8 +9,8 @@ import { isNotUser } from '../middlewares/role';
 const router = Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), article.getAll);
-router.post('/like/:id', passport.authenticate('jwt', { session: false }), article.like);
-router.get('/:slug', passport.authenticate('jwt', { session: false }), isNotUser, article.get);
+router.get('/:slug', passport.authenticate('jwt', { session: false }), article.get);
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), isNotUser, article.like);
 router.post('/:slug', passport.authenticate('jwt', { session: false }), isNotUser, checkUpdate, validateUpdate, article.update);
 router.post('/', passport.authenticate('jwt', { session: false }), isNotUser, check, validate, article.create);
 router.delete('/:slug', passport.authenticate('jwt', { session: false }), isNotUser, article.delete);
