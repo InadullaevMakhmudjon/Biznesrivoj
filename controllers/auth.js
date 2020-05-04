de95f10d2498bd3230312095b10fe57aa6b17174
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-05-04 11:04:38 +0500

    New Auth method added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import { sign } from 'jsonwebtoken';
 import { compareSync } from 'bcryptjs';
 import models from '../models';
+import service from '../services/twilio';
 
 require('dotenv').config();
 
@@ -10,6 +11,19 @@ function find(where, res, next) {
     .catch((error) => res.status(502).json(error));
 }
 
+function token({ id, res, isUser }) {
+  sign({ userId: isUser || id }, process.env.JWT_KEY, { expiresIn: '12h' }, (error, token) => {
+    if (error) res.sendStatus(501);
+    else if (isUser) res.status(200).json({ token });
+    else {
+      models.User
+        .update({ lastLoggedIn: new Date() }, { where: { id } })
+        .then(() => res.status(200).json({ token }))
+        .catch((error) => res.status(502).json(error));
+    }
+  });
+}
+
 export default {
   details(req, res) {
     delete req.user.dataValues.genderId;
@@ -17,30 +31,31 @@ export default {
     res.status(200).json({ user: req.user });
   },
   getToken(req, res) {
-    sign({ userId: 1 }, process.env.JWT_KEY, {}, (error, token) => {
-      if (error) res.sendStatus(501);
-      else {
-        res.status(200).json({ token });
-      }
-    });
+    token({ isUser: 1, res });
   },
-  login(req, res) {
+  loginByPassword(req, res) {
     if (!req.body) res.status(401).json({ message: 'Please fill the form' });
 
     const { phone, password } = req.body;
     find({ phone }, res, ([user]) => {
       if (user) {
         if (compareSync(password, user.password)) {
-          sign({ userId: user.id }, process.env.JWT_KEY, { expiresIn: '12h' }, (error, token) => {
-            if (error) res.sendStatus(501);
-            else {
-              models.User.update({ lastLoggedIn: new Date() }, { where: { id: user.id } })
-                .then(() => res.status(200).json({ token }));
-            }
-          });
+          token({ id: user.id, res });
         } else { res.status(401).json({ message: 'Password is incorrect' }); }
       } else { res.status(401).json({ message: 'Phone is not registered yet' }); }
     });
+  },
+  loginByPhone(req, res) {
+    service.verificationChecks.create({
+      to: req.phone,
+      code: req.code,
+    }).then(({ valid }) => {
+      if (valid) {
+        models.User.findOne({ where: { phone: req.phone } })
+          .then((user) => user && token({ id: user.id, res }))
+          .catch(() => res.status(502).json('Not valied'));
+      } else res.status(401).json({ message: 'Code is not confirmed' });
+    }).catch(() => res.status(502).json({ message: 'Ooops, something wend wrong' }));
   },
   logout(req, res) {
     models.User.update({
