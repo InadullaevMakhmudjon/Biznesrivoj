2b8da07132b5ae3a38b808f69206e116e1ef8ff2
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-05-21 15:10:50 +0500

    routes for Bookmark created

---
+++ routes/bookmarks.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import passport from 'passport';
+import * as bookmarks from '../controllers/bookmarks';
+import { isNotUser } from '../middlewares/role';
+
+const router = Router();
+
+router.get('/', passport.authenticate('jwt', { session: false }), bookmarks.getUserBookmarks);
+router.get('/:id', passport.authenticate('jwt', { session: false }), bookmarks.get);
+router.get('/user/:id', passport.authenticate('jwt', { session: false }), bookmarks.getUserBookmarks);
+router.get('/article/:id', passport.authenticate('jwt', { session: false }), bookmarks.getArticleBookmarks);
+router.post('/:id', passport.authenticate('jwt', { session: false }), /* isNotUser, */ bookmarks.create);
+router.delete('/', passport.authenticate('jwt', { session: false }), bookmarks.deleteAll);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), bookmarks.deleteBookmark);
+
+export default router;
