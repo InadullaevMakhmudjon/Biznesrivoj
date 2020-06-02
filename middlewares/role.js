980ed2bb90f3d911165660b91d91961d058d2fe9
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-02 11:19:51 +0500

    Docs and clients removed

--- middlewares/role.js
+++
@@ -1,8 +0,0 @@
-const validate = (roleId) => (req, res, next) => {
-  if (req.user.role.id !== roleId) next();
-  else res.status(400).json({ message: 'Not permitted' });
-};
-
-export const isNotUser = validate(3);
-
-export default isNotUser;
